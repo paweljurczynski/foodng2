2d6edb2174d15c4294bc5f8ac3a6ab8c482d82d7
Karol Kozuch <shobuiyugi@gmail.com>
2016-05-14 19:01:47 +0200

    Points

---
+++ imports/ui/admin/components/ordersList/ordersList.js
@@ -0,0 +1,58 @@
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
+import uiRouter from 'angular-ui-router';
+import {Utils} from '../../../../utils/Utils';
+
+import './ordersList.html';
+import {Orders} from '../../../../api/orders';
+
+class OrdersList {
+    constructor($scope, $reactive, OrdersService){
+        'ngInject';
+
+        $reactive(this).attach($scope);
+        this.OrdersService = OrdersService;
+
+        this.helpers({
+            orders: () => {
+                let orders = OrdersService.getOrders();
+                orders.observeChanges({
+                    added: (id, fields) => {
+                        console.log('Added');
+                    },
+                    changed: (id, fields) => {
+                        console.log('Changed');
+                    },
+                    removed: () => {
+                        console.log('doc removed');
+                    }
+                });
+                return orders;
+            }
+        });
+
+    }
+}
+
+const name = 'ordersList';
+
+// create a module
+export default angular.module(name, [
+        angularMeteor,
+        uiRouter
+    ])
+    .component(name, {
+        templateUrl: Utils.getTemplatePath(name),
+        controllerAs: 'vm',
+        controller: OrdersList
+    })
+    .config(config);
+
+function config($stateProvider) {
+    'ngInject';
+    $stateProvider
+        .state('admin.orders', {
+            url: '/Orders',
+            template: '<ordersList-list></ordersList-list>'
+        });
+}
